e89dda5647a126021e2b481619227e70fb216666
Dirk P. Janssen <djanssen@pivotal.io>
2016-03-18 23:29:20 -0700

    Set uuid on user from Angular

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,13 +1,27 @@
-app = angular.module("voat", []);
+app = angular.module("voat", ["angular-uuid", "ngCookies"]);
 
-app.controller("one", function($scope) {
+app.controller("one", ['$scope', '$http', '$cookies', 'uuid', function($scope, $http, $cookies, uuid) {
   $scope.votecolorclass = "";
+
+  $scope.determine_user = function() {
+    var userUuid = $cookies.get('useruuid');
+    if (userUuid == null) {
+      userUuid = uuid.v4(); }
+
+    var expiry = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+    $cookies.put("useruuid", userUuid, { expires: expiry });
+    };
 
   $scope.clickbutton = function(color) {
     $scope.votecolorclass = color + "-custom";
+    $http.post("/api/vote/", color).then(
+        function success(response) {
+            console.log("success call to vote", response.data); },
+        function failure(response) {
+            console.log("failed call to vote", response.data); });
   };
 
+  $scope.determine_user();
 
-
-});
+}]);
 
