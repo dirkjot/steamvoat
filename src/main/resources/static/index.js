e192bc3afb4ead89010893890dcb06dea40129ea
Dirk P. Janssen <djanssen@pivotal.io>
2016-03-19 00:05:18 -0700

    Changed datastructure and post.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,6 +1,7 @@
 app = angular.module("voat", ["angular-uuid", "ngCookies"]);
 
 app.controller("one", ['$scope', '$http', '$cookies', 'uuid', function($scope, $http, $cookies, uuid) {
+  $scope.voteData = {comment: "", color: "", uuid: ""};
   $scope.votecolorclass = "";
 
   $scope.determine_user = function() {
@@ -10,15 +11,22 @@ app.controller("one", ['$scope', '$http', '$cookies', 'uuid', function($scope, $
 
     var expiry = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
     $cookies.put("useruuid", userUuid, { expires: expiry });
+    $scope.voteDate.uuid = userUuid;
     };
 
   $scope.clickbutton = function(color) {
+    $scope.voteData.color = color;
     $scope.votecolorclass = color + "-custom";
-    $http.post("/api/vote/", color).then(
-        function success(response) {
-            console.log("success call to vote", response.data); },
-        function failure(response) {
-            console.log("failed call to vote", response.data); });
+    $scope.send();
+  };
+
+  $scope.send = function () {
+       console.log("sending", $scope.voteData);
+       $http.post("/api/vote/", $scope.voteData).then(
+            function success(response) {
+                console.log("success call to vote", response.data); },
+            function failure(response) {
+                console.log("failed call to vote", response.data); });
   };
 
   $scope.determine_user();
