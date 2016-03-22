ffd7bd0f29b39d24671777367ace6e9094fe5c83
Dirk P. Janssen <djanssen@pivotal.io>
2016-03-22 09:33:47 -0700

    Added button shake.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -30,34 +30,22 @@ app.controller("one", ['$scope', '$http', '$cookies', '$timeout', 'uuid',
       console.log("sending", $scope.voteData);
       $http.post("/vote", $scope.voteData).then(
           function success(response) {
-            console.log("success call to vote", response.data);
             if ($scope.voteData.comment != '') {
               $scope.feedbackMessage = true;
               $timeout(function() { $scope.feedbackMessage = false; },
                 3000);
             }},
           function failure(response) {
-            console.log("failed call to vote", response.data); });
+            alert("Could not contact the server, sorry");
+            console.log("failed call to vote", response.data);
+          });
     };
 
     $scope.determine_user();
 
-    //// Initialize jRumble on Selector
-    //$('.btn').jrumble({
-    //                  	x: 0,	y: 0,
-    //                  	rotation: 9, speed: 60,
-    //                  });
-    //
-    //// Start rumble on element
-    //$('.btn').trigger('startRumble');
-    //
-    //$timeout(function() { $('.btn').trigger('stopRumble'); },
-    //  1500);
-
     $timeout(function() {
-      $scope.bubbleUp = true;
-      console.log("timed");
-    }, 500);
+          $scope.bubbleUp = true;},
+        500);
 
   }]);
 
